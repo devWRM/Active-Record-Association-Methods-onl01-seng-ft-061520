cb677808eb2e15089512c1a527b099f710848f94
devWRM <mcwen@comcast.net>
2020-07-22 13:53:54 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,21 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+
+    self.songs.first.genre
+    # ex 25:15 Avi Video:   https://www.youtube.com/watch?v=5dqPYRsQd10
+    # kanye.songs.first.name
   end
 
   def song_count
     #return the number of songs associated with the artist
+
+    self.songs.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
+
+    self.genres.count
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,42 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+
+    self.artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+
+    # Google for resource: get an array of names from an ActiveRecord column
+    # Resource answer:  https://stackoverflow.com/questions/34713368/using-select-or-map-to-get-an-array-of-names-from-an-activerecord-model
+    self.artists.pluck(:name)
+    # self.artists.map(&:name)
   end
 end
+
+
+##########################################################
+
+# map(&:name) EXPLANATION below: https://www.honeybadger.io/blog/how-ruby-ampersand-colon-works/
+
+# words = ["would", "you", "like", "to", "play", "a", "game?"]
+
+### this...
+# words.map &:length
+
+### ..is equivalent to this:
+# words.map { |w| w.length }
+
+##########################################################
+
+
+
+
+# Return an array of actual Column names:   https://stackoverflow.com/questions/11011402/get-columns-names-with-activerecord
